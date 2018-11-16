760865008d19a78253f282155fa43f12b63461ee
Kodereczek <polecpatryk@gmail.com>
2018-11-16 19:43:27 +0100

    Added new activity

--- app/src/main/java/com/patrykpolec/organizer/AddObject.java
+++ app/src/main/java/com/patrykpolec/organizer/AddObject.java
@@ -1,7 +1,9 @@
 package com.patrykpolec.organizer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddObject extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class AddObject extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_object);
+    }
+
+    public void Click(View view) {
+        Intent intent;
+
+        switch (view.getId()) {
+            case R.id.button_add_task:
+                intent = new Intent(AddObject.this, AddTask.class);
+                startActivity(intent);
+                break;
+
+            case R.id.button_add_contact:
+                intent = new Intent(AddObject.this, AddContact.class);
+                startActivity(intent);
+                break;
+            case R.id.button_add_note:
+                intent = new Intent(AddObject.this, AddNote.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
